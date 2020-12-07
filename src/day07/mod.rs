4a58f80241742acb3b8f62642a39416159a0a707
hui <ihulufei@icloud.com>
2020-12-07 21:48:31 +0800

    day7 part 2

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -26,6 +26,42 @@ fn part_1(input: &str) -> usize {
     count
 }
 
+fn parse_line_2(line: &str) -> (&str, Vec<(usize, String)>) {
+    let mut iter = line.split(" bags contain ");
+    let k = iter.next().unwrap();
+    let bags = iter
+        .next()
+        .unwrap()
+        .split(", ")
+        .filter_map(|p| {
+            let mut parts = p.split_whitespace();
+            let count = parts.nth(0).and_then(|x| x.parse::<usize>().ok());
+            parts.nth_back(0);
+            let bag = parts.collect::<Vec<_>>().join(" ");
+            count.map(|x| (x, bag))
+        })
+        .collect();
+    (k, bags)
+}
+
+fn count_sum(bags: &HashMap<&str, Vec<(usize, String)>>, entry: &str) -> i32 {
+    match bags.get(entry) {
+        Some(xs) => {
+            xs.iter()
+                .map(|(count, next_entry)| (*count as i32) * count_sum(bags, next_entry))
+                .sum::<i32>()
+                + 1
+        }
+        _ => 0,
+    }
+}
+
+fn part_2(input: &str) -> i32 {
+    let bags: HashMap<&str, Vec<(usize, String)>> =
+        input.trim_end().lines().map(parse_line_2).collect();
+    count_sum(&bags, "shiny gold") - 1
+}
+
 #[test]
 fn test_part_1() {
     assert_eq!(
@@ -44,4 +80,55 @@ dotted black bags contain no other bags.
         4
     );
     assert_eq!(part_1(INPUT), 121);
+}
+
+#[test]
+fn test_parse_line_2() {
+    assert_eq!(
+        parse_line_2("light red bags contain 1 bright white bag, 2 muted yellow bags."),
+        (
+            "light red",
+            vec![
+                (1, "bright white".to_owned()),
+                (2, "muted yellow".to_owned())
+            ]
+        )
+    );
+    assert_eq!(
+        parse_line_2("faded blue bags contain no other bags."),
+        ("faded blue", vec![])
+    );
+}
+
+#[test]
+fn test_part_2() {
+    assert_eq!(
+        part_2(
+            r#"light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
+                "#
+        ),
+        32
+    );
+    assert_eq!(
+        part_2(
+            r#"shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+"#
+        ),
+        126
+    );
+    assert_eq!(part_2(INPUT), 3805);
 }
