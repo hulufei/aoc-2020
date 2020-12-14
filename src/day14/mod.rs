1316adeff842c10b6add09aa51b975586eaea6c3
hui <ihulufei@icloud.com>
2020-12-14 22:58:13 +0800

    day14 part 2

--- src/day14/mod.rs
+++ src/day14/mod.rs
@@ -28,6 +28,36 @@ fn part_1(input: &str) -> u64 {
     mem.values().sum()
 }
 
+fn apply_mask_2(mask: &str, n: u64) -> (u64, u64) {
+    // masked address
+    let n = n | u64::from_str_radix(&mask.replace("X", "0"), 2).unwrap();
+    // x masks
+    let x = n & u64::from_str_radix(&mask.replace("1", "0").replace("X", "1"), 2).unwrap();
+    (n, x)
+}
+
+fn hash_equal(a: (u64, u64), b: (u64, u64)) -> bool {
+    // remain bit should be x masks
+    a.0 ^ b.0 == a.1 & b.1
+}
+
+fn part_2(input: &str) -> u64 {
+    let mut mem = HashMap::new();
+    let mut lines = input.trim_end().lines();
+    let (_, mut mask) = parse_line(lines.next().unwrap());
+    for line in lines {
+        let (left, right) = parse_line(line);
+        if left == "mask" {
+            mask = right;
+        } else {
+            let address = left[4..(left.len() - 1)].parse::<u64>().unwrap();
+            let (masked_address, x_masks) = apply_mask_2(mask, address);
+            mem.insert(apply_mask_2(mask, address), right.parse().unwrap());
+        }
+    }
+    mem.values().sum()
+}
+
 #[test]
 fn test_apply_mask() {
     assert_eq!(apply_mask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 11), 73);
